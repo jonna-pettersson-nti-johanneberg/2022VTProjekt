36825a3979df12c038dab9cb87a7442da7e433e9
jonna-pettersson-nti-johanneberg <94617391+jonna-pettersson-nti-johanneberg@users.noreply.github.com>
2022-03-08 21:15:17 +0100

    feature: slideshow

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
-// Denna fil skall innehålla JavaScript (JS)
-console.log("hello");
+/*  Table of content
+    1. Functions
+        1.1 Slideshow
+
+    2. Event Listeners
+        2.1 Slideshow
+*/
+
+/* ==========
+    Functions
+    ========= */
+const slideShowNext = (slideShow, width) => {
+  slideShow.scrollBy({ left: width, behavior: "smooth" });
+};
+
+const slideShowPrev = (slideShow, width) => {
+  slideShow.scrollBy({ left: -width, behavior: "smooth" });
+};
+
+/* ================
+    Event Listeners
+    =============== */
+if (document.querySelector(".slide-show")) {
+  const slideShow = document.querySelector(".slide-show-elmts");
+  const width = document.querySelector(".slide-show-elmts>*").offsetWidth;
+  document.querySelector(".slide-show-next").addEventListener("click", () => {
+    slideShowNext(slideShow, width);
+  });
+  document.querySelector(".slide-show-prev").addEventListener("click", () => {
+    slideShowPrev(slideShow, width);
+  });
+}
