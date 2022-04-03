6c52d435dcf4b0c4b0a8d02269c18f3518dbea2b
jonna-pettersson-nti-johanneberg <94617391+jonna-pettersson-nti-johanneberg@users.noreply.github.com>
2022-04-03 02:26:05 +0200

    final push
    finally done

--- js/script.js
+++ js/script.js
@@ -10,6 +10,36 @@
     Functions
     ========= */
 
+const startDeselect = (name) => {
+  const deselectEl = document.querySelector(".deselect");
+  deselectEl.style.visibility = "visible";
+  deselectEl.setAttribute("name", name);
+  deselectEl.style.opacity = 0.65;
+
+  const scrollY = window.scrollY;
+  document.body.style.position = "fixed";
+  document.body.style.top = -scrollY + "px";
+};
+
+const deselect = () => {
+  const deselectEl = document.querySelector(".deselect");
+  const temp = document.querySelector(deselectEl.getAttribute("name"));
+
+  const scrollY = document.body.style.top;
+  document.body.style.position = "";
+  document.body.style.top = "";
+  window.scrollTo(0, parseInt(scrollY || "0") * -1);
+
+  if (deselectEl.getAttribute("name") == ".burger-menu") {
+    temp.style.transform =
+      window.innerWidth <= 600 ? "translateX(4em)" : "translateX(10em)";
+  } else {
+    temp.remove();
+  }
+  deselectEl.style.visibility = "hidden";
+  deselectEl.style.opacity = 0;
+};
+
 const slideShowStart = () => {
   const slideShow = document.querySelector(".slide-show-elmts");
   const width = document.querySelector(".slide-show-elmts>*").offsetWidth;
@@ -54,18 +84,27 @@ const slideShowPrev = (slideShow, width, delay) => {
 
 const burgerMenu = () => {
   menu = document.querySelector(".burger-menu");
-  deselect = document.querySelector(".deselect");
-  menu.style.transform = "translateX(-50vw)";
-  deselect.style.visibility = "visible";
-  deselect.style.opacity = 0.65;
+  menu.style.transform =
+    window.innerWidth <= 600 ? "translateX(-50vw)" : "translateX(-10em)";
+  startDeselect(".burger-menu");
 };
 
-const burgerMenuDeselect = () => {
-  menu = document.querySelector(".burger-menu");
-  deselect = document.querySelector(".deselect");
-  menu.style.transform = "translateX(50vw)";
-  deselect.style.visibility = "hidden";
-  deselect.style.opacity = 0;
+const submit = () => {
+  const box = document.createElement("div");
+  box.style.backgroundColor = "white";
+  box.style.outline = "2px solid black";
+  box.style.padding = "1ch";
+  box.style.height = "max-content";
+  box.style.maxWidth = "calc(15em - 2ch)";
+  box.style.position = "fixed";
+  box.style.top = window.innerHeight / 4 - box.offsetHeight * 2 + "px";
+  box.style.left = "calc((100vw / 2) - (15em / 2))";
+  box.style.zIndex = "100";
+  box.id = "alert";
+  box.innerHTML =
+    "Tack för ditt meddelande! <br/><br/> Vi på H&M jobbar hårt dagligen för att förbättra vårt arbete. <br/><br/> Vi återkommer så snart som möjligt! <br/> <img class='logo' src='./img/logga.png' alt='logga' style='margin-left:calc(100% - 6em - 0.5ch); margin-top: 1ch;' />";
+  document.body.appendChild(box);
+  startDeselect("#alert");
 };
 
 /* ================
@@ -88,9 +127,7 @@ if (document.querySelector(".burger-menu-btn")) {
   document
     .querySelector(".burger-menu-btn")
     .addEventListener("keydown", burgerMenu);
-  document
-    .querySelector(".deselect")
-    .addEventListener("click", burgerMenuDeselect);
+  document.querySelector(".deselect").addEventListener("click", deselect);
 }
 
 if (document.getElementsByClassName("accordion")) {
@@ -106,4 +143,8 @@ if (document.getElementsByClassName("accordion")) {
       }
     });
   }
+}
+
+if (document.querySelector(".submit")) {
+  document.querySelector(".submit").addEventListener("click", submit);
 }
